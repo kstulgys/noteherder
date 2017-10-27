f6f68cb1c634665e4cafcf1281ed62d147631b1f
Karolis Stulgys <karolis.stulgys@ci.local>
2017-10-27 15:46:58 +1100

    added main component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Main from './Main'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Noteherder
+        <Main />
       </div>
     );
   }
