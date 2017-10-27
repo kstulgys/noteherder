e75bc3a89a660c122ee5e4f3dee9fef0a0915df2
Karolis Stulgys <karolis.stulgys@ci.local>
2017-10-27 16:48:46 +1100

    styled notelist and noteform

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -5,9 +5,24 @@ import React, { Component } from 'react';
 class NoteForm extends Component {
   render() {
     return (
-      <div>
-        NOteForm
-      </div>
+      <div className="NoteForm">
+          <div className="form-actions">
+            <button type="button">
+              <i className="fa fa-trash-o"></i>
+            </button>
+          </div>
+          <form>
+            <p>
+              <input
+                type="text"
+                name="title"
+                placeholder="Title your note"
+              />
+            </p>
+            
+            <textarea name="body"></textarea>
+          </form>
+        </div>
     );
   }
 }
