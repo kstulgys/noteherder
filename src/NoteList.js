d4f505dd9a99978a8bd51647e42c41dce1aa927d
kstulgys <k.stulgys@gmail.com>
2017-10-28 21:54:18 +1100

    dynamicaly render one note from a variable

--- src/NoteList.js
+++ src/NoteList.js
@@ -1,56 +1,23 @@
 
 import React from 'react';
+import Note from './Note'
 import './NoteList.css'
 
 const NoteList = () => {
+  const note = {
+    id: 'note-4',
+    title: 'My fancy note',
+    body: 'Fancy body jadajada jada',
+  }
+
   return (
     <div className="NoteList">
-          <h3>Notes</h3>
-          <ul id="notes">
-            <a className="active">
-              <li>
-                <div className="note">
-                  <div className="note-title">
-                    Kohlrabi welsh
-                  </div>
-                  <div className="note-body">
-                    <p>
-                      Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.
-                    </p>
-                  </div>
-                </div>
-              </li>
-            </a>
-            <a>
-              <li>
-                <div className="note">
-                  <div className="note-title">
-                    Dandelion cucumber
-                  </div>
-                  <div className="note-body">
-                    <p>
-                      Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
-                    </p>
-                  </div>
-                </div>
-              </li>
-            </a>
-            <a>
-              <li>
-                <div className="note">
-                  <div className="note-title">
-                    Prairie turnip
-                  </div>
-                  <div className="note-body">
-                    <p>
-                      Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.
-                    </p>
-                  </div>
-                </div>
-              </li>
-            </a>
-          </ul>
-        </div>
+      <h3>Notes</h3>
+      <ul id="notes">
+        <Note note={note} />
+
+      </ul>
+    </div>
   );
 };
 
