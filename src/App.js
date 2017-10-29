05ae0288aaefc242dd7d171e56fe76ceb561b22d
kstulgys <k.stulgys@gmail.com>
2017-10-29 12:49:17 +1100

    use object spread for data props on app.js

--- src/App.js
+++ src/App.js
@@ -55,11 +55,15 @@ class App extends Component {
       saveNote: this.saveNote,
       removeCurrentNote: this.removeCurrentNote,
     }
+    const noteData = {
+      notes: this.state.notes,
+      currentNote: this.state.currentNote,
+    }
+
     return (
       <div className="App">
         <Main  
-        notes={this.state.notes}
-        currentNote={this.state.currentNote}
+        {...noteData}
         {...actions}
         />
 
