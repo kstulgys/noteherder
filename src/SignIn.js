62a0d75b4fc2ce98df63c77430819b86a9c753f8
Karolis Stulgys <karolis.stulgys@ci.local>
2017-10-30 11:42:00 +1100

    style signIn page

--- src/SignIn.js
+++ src/SignIn.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './SignIn.css'
 import { auth, googleProvider } from './base'
 
+
 const SignIn = () => {
 
   const authenticate = () => {
@@ -9,10 +10,27 @@ const SignIn = () => {
   }
 
   return (
-    <div className="SignIn">
-      <h1>Sign In</h1>
-      <button onClick={authenticate}>Sign In</button>
-    </div>
+      <div className="SignIn">
+        <header className="Header">
+          <img src="media/quill.svg" alt=""/>
+          <span className="title">Noteherder</span>
+        </header>
+        <main>
+          <h3>Hey, Nerd! You Like Notes?</h3>
+          <p>You never know when you'll need to write crap down. In fact, you should probably be taking notes right now.</p>
+          <button className="github">
+            <i className="fa fa-github"></i>
+            Sign in with GitHub
+          </button>
+          <button 
+            className="google"
+            onClick={authenticate}
+          >
+            <img src="media/google.svg" alt=""/>
+            Sign in with Google
+          </button>
+        </main>
+      </div>
   );
 };
 
